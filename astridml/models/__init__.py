25818bc4376d72edf76c3335729064ec0099f6e9
Shelby-Fulton <shelby.fulton7@gmail.com>
2025-12-10 00:18:01 -0500

    recommendation engine

--- astridml/models/__init__.py
+++ astridml/models/__init__.py
@@ -1,5 +1,6 @@
 """Machine learning models for AstridML."""
 
 from astridml.models.predictor import SymptomPredictor
+from astridml.models.recommender import RecommendationEngine
 
-__all__ = ["SymptomPredictor"]
+__all__ = ["SymptomPredictor", "RecommendationEngine"]
